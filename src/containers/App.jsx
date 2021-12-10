b07b9ed3f0952796054bb7f78260fe76655aa576
Maksim Solopov <mxsolopov@yandex.ru>
2021-12-10 16:33:57 +0300

    Unused component fixed

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-// import { addCashAction, getCashAction, addCustomerAction, removeCustomerAction } from '../actions/actions';
 import { Container } from 'reactstrap';
 import Navigation from '../components/Navigation';
 import Page from './Page';
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const cash = useSelector(state => state.cash.cash);
-  // const customers = useSelector(state => state.customers.customers);
-
-  // const addCash = (cash) => {
-  //   dispatch(addCashAction(cash));
-  // };
-
-  // const getCash = (cash) => {
-  //   dispatch(getCashAction(cash));
-  // };
-
-  // const addCustomer = (name) => {
-  //   const customer = {
-  //     name,
-  //     id: Date.now(),
-  //   }
-  //   dispatch(addCustomerAction(customer));
-  // }
-
-  // const removeCustomer = (id) => {
-  //   dispatch(removeCustomerAction(id));
-  // }
 
   return (
     <Container fluid="sm">
